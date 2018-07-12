c49293cb018d44e21956e26b6bab02c744931596
AnilJava1 <amahabali@hotmail.com>
2018-07-12 13:27:08 +0200

    first commit

---
+++ ZeeSlag.java
@@ -0,0 +1,96 @@
+package ZeeSlag;
+
+import java.util.Scanner;
+
+public class ZeeSlag {
+
+
+	public static void main(String[] args) {
+		System.out.println("Kies een waarde");
+		Scanner scanner = new Scanner(System.in);
+		String antwoord = scanner.nextLine();
+		String zee = "00111001111";
+	//	System.out.println(zee.charAt(2));
+		
+		if(zee.charAt(0) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		
+		if(zee.charAt(1) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		
+		if (zee.charAt(2) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		
+		if (zee.charAt(3) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		
+		if (zee.charAt(4) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		
+		if (zee.charAt(5) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		
+		if (zee.charAt(6) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		if (zee.charAt(7) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		if (zee.charAt(8) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		if (zee.charAt(9) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		if (zee.charAt(10) == '1') {
+			System.out.println("Boem");
+		}else {
+			System.out.println("Plons");
+		}
+		//System.out.println(antwoord + " Dat is fout.");	
+		
+
+	}
+
+
+	}
+}
+
+class boot{
+	
+}
+
+
+/*
+Werk stap voor stap, beschrijf elk miniscuul stapje.
+1: ik heb een boot nodig --> maak een boot.
+2: ik heb een scanner nodig.
+3
+2: ik heb een plek nodig waar de boot op staat. --> Maak de plek
+
+*/
